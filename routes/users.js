88afe28bdace53adfbc96bdbee863142925c4a69
Bill Miller <bill.miller415@gmail.com>
2020-07-06 18:23:53 -0600

    users controller

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var usersController = require('../controllers/usersController')
+
+router.get('/login', usersController.login)
 
 module.exports = router;
