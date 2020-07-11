5efe916a4416c27d80074db7b74721b36fab486e
Bill Miller <bill.miller415@gmail.com>
2020-07-11 13:50:07 -0600

    show errors in layout

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,22 +6,23 @@ exports.create = (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() })
+        res.render('createUser.pug', { 
+            title: 'create user',
+            errors: errors.array()
+        })
+    } else {
+        const User = require('../models/user.js')
+
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        })
+        user.save((err) => {
+            // handle err
+
+            res.render('userCreated.pug', { title: 'user created' })
+        })
     }
-
-    const User = require('../models/user.js')
-
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
-    user.save((err) => {
-        if (err) {
-            return next(err)
-        }
-
-        res.render('userCreated.pug', { title: 'user created' })
-    })
 }
 exports.login = (req, res) => res.send('login')
 
