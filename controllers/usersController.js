5fa350ed0eead42c9295d3c60b22d9cf9a9e54ee
billmiller2 <billchill67@gmail.com>
2020-07-13 22:33:53 -0600

    bcrypt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator')
 const { User } = require('../models/user.js')
+const bcrypt = require('bcrypt')
 
 exports.createUserForm = (req, res) => res.render('createUser.pug', { title: 'create user' })
 
@@ -7,27 +8,31 @@ exports.create = (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
-        res.render('createUser.pug', { 
+        res.render('createUser.pug', {
             title: 'create user',
             errors: errors.array()
         })
     } else {
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password
-        })
-        user.save((err) => {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
             // handle err
 
-            req.session.sessionFlash = {
-                type: 'success',
-                message: 'User Created'
-            }
+            const user = new User({
+                username: req.body.username,
+                password: hash
+            })
 
-            res.redirect(301, '/users/login')
+            user.save((err) => {
+                // handle err
+
+                req.session.sessionFlash = {
+                    type: 'success',
+                    message: 'User Created'
+                }
+
+                res.redirect(301, '/users/login')
+            })
         })
     }
 }
 
 exports.login = (req, res) => res.render('login.pug', { title: 'raresnaps login' })
-
