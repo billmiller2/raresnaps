27c5cd46f3afc37d8722ae93ee3e24bcee1d54ac
Bill Miller <bill.miller415@gmail.com>
2020-07-18 13:27:53 -0600

    authenticate middleware

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,9 +1,3 @@
 exports.index = (req, res) => {
-    const token = req.cookies.token
-
-    if (token) {
-        res.render('index.pug')
-    } else {
-        res.redirect(301, '/users/login')
-    }
+    res.render('index.pug')
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const appController = require('../controllers/appController')
+const authenticate = require('../lib/middleware/auth')
 
-router.get('/', appController.index)
+router.get('/', authenticate, appController.index)
 
 module.exports = router;
