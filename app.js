dc688b20509e14dc3b774bd88776dc6c87d2c87e
billmiller2 <billchill67@gmail.com>
2020-07-26 23:45:25 -0600

    set up react

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public/stylesheets', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/public/js', express.static(__dirname + '/node_modules/react'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ frontend/root/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Root = ()  => {
+    return (
+        <h1 className='container'>
+            raresnaps
+        </h1>
+    )
+}
