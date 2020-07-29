84cd6abe7bb1fc5ba1da909cd1a3f0650b76dd36
billmiller2 <billchill67@gmail.com>
2020-07-28 22:58:17 -0600

    redux

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import { Provider } from 'react-redux'
+import { store } from './store'
+
 import { Root } from './root/root.jsx'
 
-render(<Root/>, document.getElementById('app'))
+render(
+    <Provider store={store}>
+        <Root/>
+    </Provider>,
+    document.getElementById('app')
+)
