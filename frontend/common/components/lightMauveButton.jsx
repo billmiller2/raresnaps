723b731add3aff6db61f943eeffe8e1f8df3e12e
Bill Miller <bill.miller415@gmail.com>
2020-09-21 19:13:18 -0600

    light mauve button box shadow

--- frontend/common/components/lightMauveButton.jsx
+++ frontend/common/components/lightMauveButton.jsx
@@ -9,5 +9,22 @@ export const LightMauveButton = styled(Button)`
         background-color: #a17188;
         color: #ffffff;
         border-color: #a17188;
+        box-shadow: 0 0 10px !important;
+    }
+    &:active {
+        &:focus {
+            background-color: #a17188 !important;
+            box-shadow: 0 0 10px !important;
+        }
+        background-color: #a17188 !important;
+        border: 1px solid !important;
+        color: #a17188 !important;
+        box-shadow: 0 0 10px !important;
+    }
+    &:focus {
+        background-color: #a17188 !important;
+        border: 1px solid !important;
+        color: #ffffff !important;
+        box-shadow: 0 0 10px !important;
     }
 `
