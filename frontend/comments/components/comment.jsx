419dfc7139ea3ffeb3a5db4ec2445ebaa7959937
Bill Miller <bill.miller415@gmail.com>
2020-09-24 18:33:35 -0600

    style comments

--- frontend/comments/components/comment.jsx
+++ frontend/comments/components/comment.jsx
@@ -3,10 +3,16 @@ import styled from 'styled-components'
 
 import { decodeHtml } from '../../common'
 
+const Div = styled.div`
+    border: 1px solid #f3e9e3;
+    color: #a17188;
+    padding: 15px;
+`
+
 export const Comment = (props) => {
     const { comment } = props
 
     return (
-        <div className='mr-1 mb-1'>{decodeHtml(comment.comment)}</div>
+        <Div className='mr-1 mb-3'>{decodeHtml(comment.comment)}</Div>
     )
 }
