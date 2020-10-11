d14d80549d62045e4dc4b3ffbb7dc8dea851ae57
Bill Miller <bill.miller415@gmail.com>
2020-10-11 15:36:00 -0600

    db auth

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb://127.0.0.1:27017/raresnaps-dev'
+var mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:27017/raresnaps-dev`
 mongoose.connect(mongoDB, { useNewUrlParser: true })
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
