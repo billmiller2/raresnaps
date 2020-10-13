c9adeee29dab9a806255fcb188602d00698266ec
Bill Miller <bill.miller415@gmail.com>
2020-10-12 20:54:40 -0600

    initial error log

--- app.js
+++ app.js
@@ -69,6 +69,11 @@ app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+    fs.writeFile('./errors.log', err, function(err, data) {
+        if (err) {
+            console.log(err)
+        }
+    });
 
     // render the error page
     res.status(err.status || 500);
