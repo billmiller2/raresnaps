b93b9229af8ce0a91021ef63c5320cfdfcdac307
Bill Miller <bill.miller415@gmail.com>
2020-10-12 20:41:50 -0600

    access log

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('express-session')
 var logger = require('morgan');
+var fs = require('fs')
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
@@ -24,6 +25,9 @@ var db = require('./db.js')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(logger('common', {
+    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
