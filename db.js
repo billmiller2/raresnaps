7df1a0a4f080a7c805629b94baab08e38ae5c222
Bill Miller <bill.miller415@gmail.com>
2020-10-13 19:02:25 -0600

    require dotenv in db.js

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+
 var mongoose = require('mongoose')
 var mongoDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:27017/raresnaps-dev`
 mongoose.connect(mongoDB, { useNewUrlParser: true })
