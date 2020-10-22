397f1f6496a05217e3ec7f402d66f6c03a45587b
Bill Miller <bill.miller415@gmail.com>
2020-10-22 17:18:39 -0600

    jest config

--- frontend/comments/components/comment.test.js
+++ frontend/comments/components/comment.test.js
@@ -3,3 +3,10 @@ import { shallow } from 'enzyme'
 
 import { Comment } from './comment.jsx'
 
+describe('Comment', () =. {
+    it('renders a comment', () => {
+        const wrapper = shallow(<Comment />)
+        expect(wrapper.find(Comment)).to.have.lengthOf(1)
+    })
+})
+
