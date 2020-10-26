c596e450a2d4922f783a8f53e6131d35827be461
billmiller2 <billchill67@gmail.com>
2020-10-25 21:49:00 -0600

    remove padding

--- frontend/photos/components/add.jsx
+++ frontend/photos/components/add.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import * as Ladda from 'ladda'
 import styled from 'styled-components'
 
@@ -26,8 +26,8 @@ export const AddPhoto = (props) => {
     return (
         <Row>
             <Col xs={12}>
-                <form 
-                    encType='multipart/form-data' 
+                <form
+                    encType='multipart/form-data'
                     onSubmit={(e) => {
                         const submit = Ladda.create(document.querySelector('#addPhotoButton'))
                         e.preventDefault()
@@ -40,20 +40,22 @@ export const AddPhoto = (props) => {
                             })
                     }}>
                     <div className='d-flex justify-content-center'>
-                        <LightMauveButton>
-                        <Input
-                            type='file'
-                            accept="image/png, image/jpeg"
-                            onChange={(e) => setValue(e.target.files[0])}
-                            required />
-                        </LightMauveButton>
-                        <LightMauveButton 
-                            className='ml-1 ladda-button' 
-                            data-style='expand-right'
-                            id='addPhotoButton'
-                            type="submit">
-                            Upload Photo
-                        </LightMauveButton>
+                        <Row>
+                            <LightMauveButton className='mb-3'>
+                            <Input
+                                type='file'
+                                accept="image/png, image/jpeg"
+                                onChange={(e) => setValue(e.target.files[0])}
+                                required />
+                            </LightMauveButton>
+                            <LightMauveButton
+                                className='ml-1 mb-3 ladda-button'
+                                data-style='expand-right'
+                                id='addPhotoButton'
+                                type="submit">
+                                Upload Photo
+                            </LightMauveButton>
+                        </Row>
                     </div>
                 </form>
             </Col>
