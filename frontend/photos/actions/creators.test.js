34b87fdec429b4a39f816d5fe7597cbe57cfa540
Bill Miller <bill.miller415@gmail.com>
2020-10-30 18:46:17 -0600

    test async fetch photo

--- frontend/photos/actions/creators.test.js
+++ frontend/photos/actions/creators.test.js
@@ -1,7 +1,50 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+
 import * as actions from './creators.js'
 import * as types from './types.js'
+import { PHOTO } from '../routes'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe('creators', () => {
+    afterEach(() => {
+        fetchMock.restore()
+    })
+
+    it('creates RECEIVE_PHOTO after photo has been fetched', () => {
+        const photoId = '123'
+
+        const photo = { 
+            comments: [],
+            data: 'someBase64',
+            tags: []
+        }
+
+        const expectedPayload = {
+            photos: {
+                [photoId]: photo
+            }
+        }
+
+        fetchMock.getOnce(PHOTO + photoId, {
+            body: expectedPayload
+        })
+
+        const expectedActions = [
+            { type: types.REQUEST_PHOTO },
+            { type: types.RECEIVE_PHOTO, payload: expectedPayload },
+        ]
+
+        const store = mockStore({ photos: {} })
+
+        return store.dispatch(actions.fetchPhoto(photoId)).then(() => {
+            expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
+
     it('should create an request photos action', () => {
         const expectedAction = {
             type: types.REQUEST_PHOTOS
